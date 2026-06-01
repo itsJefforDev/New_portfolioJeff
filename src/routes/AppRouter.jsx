30ce20c1ef0413621452c26f8810502219e5f282
Jeff <jeffersonorozco24@gmail.com>
2026-06-01 18:43:40 -0500

    FEAT: add cv section

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home";
 import About from "../pages/About";
+import Cv from "../pages/Cv";
 import NotFound from "../pages/NotFound";
 
 function AppRouter() {
@@ -11,6 +12,7 @@ function AppRouter() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/Cv" element={<Cv />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </MainLayout>
